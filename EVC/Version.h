bb5f7aeb54acea126c52e1bc0239b4065f09000b
IHateGameDev <ihategamedev@proton.me>
2025-05-31 15:29:23 +0000

    Fix: build tests

--- EVC/Version.h
+++ EVC/Version.h
@@ -7,14 +7,12 @@
 #define EVC_VERSION_H
 
 #define EVC_VERSION_PREFIX "a"
-#define EVC_VERSION_MAJOR 0
-#define EVC_VERSION_MINOR 0
-#define EVC_VERSION_PATCH 0
+#define EVC_VERSION_MAJOR  0
+#define EVC_VERSION_MINOR  0
+#define EVC_VERSION_PATCH  0
 
-#define EVC_VERSION \
-  #EVC_VERSION_PREFIX \
-  #EVC_VERSION_MAJOR "." \
-  #EVC_VERSION_MINOR "." \
-  #EVC_VERSION_PATCH
+#define EVC_VERSION                                                            \
+  #EVC_VERSION_PREFIX #EVC_VERSION_MAJOR "." #EVC_VERSION_MINOR                \
+                                         "." #EVC_VERSION_PATCH
 
 #endif // !EVC_VERSION_H
